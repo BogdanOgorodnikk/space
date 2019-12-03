03ef8c7889476d9fdf2b6300d71355ab9eaf1aca
BogdanOgorodnikk <superpm7@gmail.com>
2019-12-03 17:07:17 +0200

    new slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-$(".banner__slider").owlCarousel({
+$(document).ready(function(){
+  $(".banner__slider").owlCarousel({
     items: 1,
     nav: true,
     navText: ["&#9668;","&#9658;"],
     loop: true
   });
+  $(".bar__slider").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: ["",""],
+    loop: true
+  });
+});
