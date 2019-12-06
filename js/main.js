e48f45a799e251b3075bc98e7d2327d2fa2733a2
BogdanOgorodnikk <superpm7@gmail.com>
2019-12-06 20:08:11 +0200

    product page

--- js/main.js
+++ js/main.js
@@ -11,6 +11,12 @@ $(document).ready(function(){
     navText: ["",""],
     loop: true
   });
+  $(".product-banner__slider").owlCarousel({
+    items: 3,
+    nav: true,
+    navText: ["&#9668;","&#9658;"],
+    loop: true
+  });
   $('.elya__head').on('click', function(){
     var find = $(this).next('.elya__text');
     find.slideToggle();
