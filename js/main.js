93d85bd7de3ff9cbfad337657332c383d0b814d6
BogdanOgorodnikk <superpm7@gmail.com>
2019-12-09 19:37:16 +0200

    responsive 1

--- js/main.js
+++ js/main.js
@@ -21,4 +21,27 @@ $(document).ready(function(){
     var find = $(this).next('.elya__text');
     find.slideToggle();
   });
+  $('.product-hot__recent').hide();
+
+
+  $('.product-hot__item--button').on('click', function() {
+    $(this).addClass('product-hot__item--active');
+    $(this).removeClass('product-hot__item--disactive');
+    $(this).prevAll().removeClass('product-hot__item--active');
+    $(this).nextAll().removeClass('product-hot__item--active');
+    $(this).prevAll().addClass('product-hot__item--disactive');
+    $(this).nextAll().addClass('product-hot__item--disactive');
+  });
+
+  $('.product-hot__item--button').on('click', function() {
+    var get_id = this.id;
+    var get_current = $('.product-hot__general-box .' + get_id);
+
+    $('.product-hot__type').not(get_current).hide(500);
+    get_current.show(500);
+  });
+  $(".btn-nav").on("click", function() {
+    var target = $(this).data("target");
+    $(target).toggleClass("nav__list--open");
+});
 });
